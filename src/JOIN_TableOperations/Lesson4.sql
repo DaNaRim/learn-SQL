51bb19adfeb2a9da289a48d219afbd0fa13bb2bc
DaNaRim <rangar.danarim@gmail.com>
2020-04-21 16:13:35 +0300

    Block 3 Lesson 4

---
+++ src/JOIN_TableOperations/Lesson4.sql
@@ -0,0 +1,16 @@
+--INSERT INTO
+
+--INSERT INTO table_name
+--VALUES (value1, value2, value3,...);
+
+INSERT INTO Employees
+VALUES (8, 'Anthony', 'Young', 35);
+
+--INSERT INTO table_name (column1, column2, column3, ..., columnN)
+--VALUES (value1, value2, value3, ..., valueN);
+
+INSERT INTO Employees (ID, FirstName, LastName, Age)
+VALUES (8, 'Anthony', 'Young', 35);
+
+INSERT INTO Employees (ID, FirstName, LastName)
+VALUES (9, 'Samuel', 'Clark');
